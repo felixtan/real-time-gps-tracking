a914fd9526440ec31781607f252cdf8cb006421a
felixtan <tanf91@gmail.com>
2015-09-02 05:03:56 -0400

    integrated socket.io; posts to '/' send data to frontend; abandon db persistence

--- index.js
+++ index.js
@@ -5,16 +5,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var app = express();
-
-// Flightaware API
-var util = require('util');
-var restclient = require('restler');
-var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
-var username = 'tanf91';
-var apiKey = 'e81d2f8cf373e02c2f8a83c629b31eb5a252cc2e';
-
-// backlog of coords to be plotted
-var coords = [];
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // jade
 app.set('view engine', 'jade');
@@ -26,43 +18,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// used to get coords from flightaware api
-// setInterval(function() {
-//     console.log('Getting flight info... %s', new Date());
-//     restclient.get(fxml_url + 'GetLastTrack', {
-//         username: username,
-//         password: apiKey,
-//         query: { ident: 'DAL407', howMany: 1 }
-//     }).on('success', function(result, response) {
-//         fs.writeFileSync('coords.txt', JSON.stringify(result.GetLastTrackResult.data), 'utf8', function(err) {
-//             if(err) return console.log(err);
-//             console.log('data > coords.txt')
-//         });
-//     });
-// }, 10000);
-
-fs.readFile('coords.txt', 'utf8', function(err, data) {
-    if(err) throw err;
-
-    data = JSON.parse(data);
-    coords = data;
-});
-
 app.get('/', function(req, res) {
-    res.render('index', { coords: coords });
+    res.render('index');
 });
 
 // assuming incoming data is json and includes lat/long and time fields
-app.post('/', function(req, res) {
-
-});
-
-// www
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-    console.log('connected to websockets');
+app.post('/', function(req, res, next) {
+    var d = new Date();
+    var coord = {
+        'time': req.body.time || d.getTime(),
+        'lat': req.body.lat,
+        'long': req.body.long
+    };
+    io.sockets.emit('reading', coord);
+    res.send({});
 });
 
 server.listen(3000, function() {
